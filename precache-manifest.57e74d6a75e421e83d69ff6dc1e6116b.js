d38aef824770a2bd4b7b706a8d8cad859b599faf
Fahd Rahali <rahali.fahd@gmail.com>
2021-02-21 08:25:24 +0100

    Updates

---
+++ precache-manifest.57e74d6a75e421e83d69ff6dc1e6116b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "3351869e671b60d6771c",
+    "url": "/react-todo-list.git/static/css/main.6b410aeb.chunk.css"
+  },
+  {
+    "revision": "3351869e671b60d6771c",
+    "url": "/react-todo-list.git/static/js/main.828942a5.chunk.js"
+  },
+  {
+    "revision": "43c5398921c2c4accc48",
+    "url": "/react-todo-list.git/static/js/runtime~main.fe3d79c0.js"
+  },
+  {
+    "revision": "6406579f6f8ab5c5fa9a",
+    "url": "/react-todo-list.git/static/css/2.8cfa6922.chunk.css"
+  },
+  {
+    "revision": "6406579f6f8ab5c5fa9a",
+    "url": "/react-todo-list.git/static/js/2.2266ed54.chunk.js"
+  },
+  {
+    "revision": "7a4d9ac624a0c3d9c4b5",
+    "url": "/react-todo-list.git/static/js/3.9c2c8f1a.chunk.js"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/react-todo-list.git/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/react-todo-list.git/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/react-todo-list.git/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/react-todo-list.git/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "d7c639084f684d66a1bc66855d193ed8",
+    "url": "/react-todo-list.git/static/media/fontawesome-webfont.d7c63908.svg"
+  },
+  {
+    "revision": "8597aeb978339cfc471b2437127dc979",
+    "url": "/react-todo-list.git/index.html"
+  }
+];
